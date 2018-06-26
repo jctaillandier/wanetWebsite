3f06bd03d5055b2e483be2c780c68f4c49bef030
J-C <jc.taillandier@hotmail.com>
2018-06-27 07:46:48 +0800

    Cyril added to founders

---
+++ js/stuff (2018_06_22 04_34_04 UTC).js
@@ -0,0 +1,10 @@
+/**
+ * To make internalhref go a little higher upon click
+ * window.addEventListener("hashchange", function () {
+    window.scrollTo(window.scrollX, window.scrollY - 100);
+    });
+
+    Also smooth internal ref movement
+
+    Also hide navbar upon click when mobile mode 
+ */
