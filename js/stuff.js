0b931fa29c1831ead8814b37bd7a7acb59ef3202
J-C <jc.taillandier@hotmail.com>
2018-07-04 09:08:37 +0800

    some JS added

--- js/stuff.js
+++ js/stuff.js
@@ -9,3 +9,28 @@
     Also hide navbar upon click when mobile mode 
  */
 
+
+ //no effect
+var downBtn = document.querySelector("#downBtn");
+
+downBtn.addEventListener("mouseover", function(){
+    downBtn.classList.add("downBtnShade");
+});
+downBtn.addEventListener("mouseleave" , function(){
+    this.classList.remove("downBtnShade");
+});
+
+
+var menuItems = document.querySelectorAll("a.nav-link");
+
+
+for(var i = 0 ; i < menuItems.length ; i ++){
+    menuItems[i].addEventListener("mouseover" , function(){
+        this.classList.add("menuItemShade");
+    })
+}
+for(var i = 0 ; i < menuItems.length ; i ++){
+    menuItems[i].addEventListener("mouseleave" , function(){
+        this.classList.remove("menuItemShade");
+    })
+}
